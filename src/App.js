4fc40e044552974c04fcb048992ea2705cebf08c
jcowman2 <joe.cowman@huskers.unl.edu>
2020-04-18 01:36:16 -0500

    Play/pause

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from "react";
 import Stage from "./components/Stage";
 
+const LOOP_INTERVAL = 10;
+
 function App() {
   const [time, setTime] = React.useState(0);
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const [loop, setLoop] = React.useState();
 
   return (
     <div>
       <Stage time={time} />
-      <button onClick={() => setTime(t => t + 50)}>Time: {time}</button>
+      <button
+        onClick={() => {
+          if (isPlaying) {
+            clearInterval(loop);
+            setLoop(null);
+            setIsPlaying(false);
+          } else {
+            setLoop(
+              setInterval(() => setTime(t => t + LOOP_INTERVAL), LOOP_INTERVAL)
+            );
+            setIsPlaying(true);
+          }
+        }}
+      >
+        {isPlaying ? "Pause" : "Play"}
+      </button>
     </div>
   );
 }
--- src/components/Stage.js
+++ src/components/Stage.js
@@ -5,49 +5,6 @@ import { Engine, Render, Bodies, World } from "matter-js";
  * Props:
  * - time
  */
-/*
-function Stage(props) {
-  const { time } = props;
-
-  const stage = React.createRef();
-  const engine = React.createRef(() => Engine.create());
-  const render = React.createRef(() =>
-    Render.create({ element: stage, engine })
-  );
-
-  const [lastRenderedTime, setLastRenderedTime] = React.useState(0);
-
-  React.useEffect(() => {
-    if (!engine.current) {
-      return;
-    }
-    const box = Bodies.rectangle(400, 200, 80, 80);
-    const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-
-    World.add(engine.current.world, [box, ground]);
-  }, [engine]);
-
-  React.useEffect(() => {
-    if (time > lastRenderedTime) {
-      const delta = time - lastRenderedTime;
-      setLastRenderedTime(delta);
-      console.log("update", delta);
-      Engine.update(engine.current, delta);
-    }
-  }, [time, lastRenderedTime, engine]);
-
-  React.useEffect(() => {
-    if (render.current) {
-      Render.world(render.current);
-    }
-  }, [render]);
-
-  return <div ref={stage} />;
-}
-
-export default Stage;
-*/
-
 export default class Stage extends React.Component {
   state = {
     lastRenderedTime: 0
@@ -75,7 +32,6 @@ export default class Stage extends React.Component {
       this.props.time > this.state.lastRenderedTime
     ) {
       const delta = this.props.time - this.state.lastRenderedTime;
-      console.log("update", delta);
       Engine.update(this.engine, delta);
       Render.world(this.gameRender);
       this.setState({ lastRenderedTime: this.props.time });
