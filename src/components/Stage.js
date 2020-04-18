bf8e46f5f8408345b9b0f7238ff14f2c514ab408
jcowman2 <joe.cowman@huskers.unl.edu>
2020-04-18 01:30:07 -0500

    WIP - hooks

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -5,6 +5,49 @@ import { Engine, Render, Bodies, World } from "matter-js";
  * Props:
  * - time
  */
+/*
+function Stage(props) {
+  const { time } = props;
+
+  const stage = React.createRef();
+  const engine = React.createRef(() => Engine.create());
+  const render = React.createRef(() =>
+    Render.create({ element: stage, engine })
+  );
+
+  const [lastRenderedTime, setLastRenderedTime] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!engine.current) {
+      return;
+    }
+    const box = Bodies.rectangle(400, 200, 80, 80);
+    const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+    World.add(engine.current.world, [box, ground]);
+  }, [engine]);
+
+  React.useEffect(() => {
+    if (time > lastRenderedTime) {
+      const delta = time - lastRenderedTime;
+      setLastRenderedTime(delta);
+      console.log("update", delta);
+      Engine.update(engine.current, delta);
+    }
+  }, [time, lastRenderedTime, engine]);
+
+  React.useEffect(() => {
+    if (render.current) {
+      Render.world(render.current);
+    }
+  }, [render]);
+
+  return <div ref={stage} />;
+}
+
+export default Stage;
+*/
+
 export default class Stage extends React.Component {
   state = {
     lastRenderedTime: 0
@@ -24,12 +67,6 @@ export default class Stage extends React.Component {
     const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
     World.add(this.engine.world, [box, ground]);
-    // Engine.run(this.engine);
-    // Engine.run(this.engine);
-    // Render.world(this.gameRender);
-    // Engine.update(this.engine, 0);
-    // Engine.run(this.engine);
-    // Render.run(this.render);
   }
 
   componentDidUpdate(prevProps) {
@@ -46,20 +83,6 @@ export default class Stage extends React.Component {
   }
 
   render() {
-    // if (!this.render.world) {
-    //   return null;
-    // }
-
-    // if (this.state.lastRenderedTime < this.props.time) {
-    //   Engine.update(this.engine, this.props.time - this.state.lastRenderedTime);
-    //   this.setState({ lastRenderedTime: this.props.time });
-    // }
-
-    // if (this.gameRender) {
-    //   Render.world(this.gameRender);
-    // }
-
-    // Render.world(this.engine);
     return <div ref="stage" />;
   }
 }
