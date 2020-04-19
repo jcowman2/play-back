9213732d0b69a46c586a541939ba08efd61bb85f
jcowman2 <joe.cowman@huskers.unl.edu>
2020-04-19 15:05:14 -0500

    Set overlay with hooks

--- src/App.js
+++ src/App.js
@@ -4,45 +4,68 @@ import { PLAYABLE_LEVELS } from "./data/levels";
 import AdminKeyHandler from "./components/AdminKeyHandler";
 import "./CRT.scss";
 
-function App() {
-  const [levelIdx, setLevelIdx] = React.useState(0);
-  const [level, setLevel] = React.useState(() =>
-    PLAYABLE_LEVELS[levelIdx].level()
-  );
-  const [animate, setAnimate] = React.useState(true);
+const useCRT = () => {
+  const [animate, setAnimate] = React.useState(false);
+  const [overlayText, setOverlayText] = React.useState(null);
 
+  const [overlayTextDuration, setOverlayTextDuration] = React.useState(3000);
+
+  React.useEffect(() => {
+    if (!overlayText || overlayTextDuration < 0) {
+      return;
+    }
+    setTimeout(() => setOverlayText(null), overlayTextDuration);
+  }, [overlayText, overlayTextDuration]);
+
+  const monitorClass = "Monitor" + (animate ? " TurnOn" : "");
+
+  return {
+    setAnimate,
+    monitorClass,
+    overlayText,
+    setOverlayText,
+    setOverlayTextDuration
+  };
+};
+
+const formatLevelName = idx => `${idx + 1}: ${PLAYABLE_LEVELS[idx].name}`;
+
+function App() {
+  const [levelIdx, setLevelIdx] = React.useState();
+  const [level, setLevel] = React.useState();
   const [, forceUpdate] = React.useState(0);
+  const { setAnimate, monitorClass, overlayText, setOverlayText } = useCRT();
 
   const toLevel = idx => {
-    level.teardown();
+    if (level) {
+      level.teardown();
+    }
+
     setLevelIdx(idx);
     setLevel(PLAYABLE_LEVELS[idx].level());
     setAnimate(true);
+    setOverlayText(formatLevelName(idx));
   };
 
   const handleEnterGoal = () => {
     toLevel(levelIdx + 1);
   };
 
-  const overlayString = `${levelIdx + 1}: ${PLAYABLE_LEVELS[levelIdx].name}`;
-  const monitorClass = "Monitor" + (animate ? " TurnOn" : "");
-  const overlayClass = "Overlay" + (animate ? " OverlayOn" : "");
-
   return (
     <div className="App">
-      <div className={monitorClass}>
+      <div className={monitorClass} onAnimationEnd={() => setAnimate(false)}>
         <div className="Screen">
-          <Level
-            data={level}
-            onUpdateData={() => {
-              forceUpdate(f => f + 1);
-            }}
-            onEnterGoal={handleEnterGoal}
-          />
+          {level && (
+            <Level
+              data={level}
+              onUpdateData={() => {
+                forceUpdate(f => f + 1);
+              }}
+              onEnterGoal={handleEnterGoal}
+            />
+          )}
         </div>
-        <div className={overlayClass} onAnimationEnd={() => setAnimate(false)}>
-          {overlayString}
-        </div>
+        <div className="Overlay">{overlayText}</div>
       </div>
 
       <AdminKeyHandler onSetLevel={toLevel} />
